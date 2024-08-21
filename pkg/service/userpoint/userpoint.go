373060de10cd10785fde0bf0963e45b284a56efb
qwp8510 <a4793706@gmail.com>
2024-08-22 00:23:33 +0800

    fix: UpsertForUserTask fix db name case intensive

--- pkg/service/userpoint/userpoint.go
+++ pkg/service/userpoint/userpoint.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"time"
+	"tradingAce/pkg/utils"
 )
 
 type Manager struct {
@@ -13,13 +14,13 @@ type Manager struct {
 
 func (m *Manager) UpsertForUserTask(ctx context.Context, address string, taskId string, point int) error {
 	query := `
-		INSERT INTO userPoint ("id", "userAddress", "taskId", "point", "createdAt")
+		INSERT INTO "userPoint" ("id", "userAddress", "taskId", "point", "createdAt")
 		VALUES ($1, $2, $3, $4, $5)
 		ON CONFLICT ("userAddress", "taskId")
 		DO UPDATE SET "point" = EXCLUDED."point"
 	`
 
-	_, err := m.db.ExecContext(ctx, query, address, taskId, point, time.Now())
+	_, err := m.db.ExecContext(ctx, query, utils.GenDBID(), address, taskId, point, time.Now())
 	if err != nil {
 		return fmt.Errorf("UpsertForUserTask failed to upsert user task: %v", err)
 	}
