4e475aa8c03c8683c6ef130e5b2f1d2d59ffac97
qwp8510 <a4793706@gmail.com>
2024-08-22 00:39:34 +0800

    test: add test usertask for upsert

---
+++ pkg/service/usertask/usertask_test.go
@@ -0,0 +1,112 @@
+package usertask
+
+import (
+	"context"
+	"testing"
+	"tradingAce/internal/testutils"
+	"tradingAce/pkg/model"
+
+	"github.com/joho/godotenv"
+	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestManager_upsert(t *testing.T) {
+	godotenv.Load("../../../.env/.env")
+
+	d, err := testutils.GetTestDb(t, "../../../migrations")
+	if err != nil {
+		t.Errorf("setup db err: %v", err)
+		return
+	}
+	defer d.Close()
+
+	type args struct {
+		ctx     context.Context
+		address string
+		taskId  string
+		state   string
+		amount  decimal.Decimal
+	}
+	tests := []struct {
+		name string
+		args args
+		want model.UserTask
+	}{
+		{
+			name: "upsert not exist usertask",
+			args: args{
+				ctx:     context.TODO(),
+				address: "0x0000000000000000000000000000000000000000",
+				taskId:  "task1",
+				state:   "pending",
+				amount:  decimal.NewFromInt(10),
+			},
+			want: model.UserTask{
+				UserAddress: "0x0000000000000000000000000000000000000000",
+				TaskID:      "task1",
+				State:       "pending",
+				Amount:      decimal.NewFromInt(10),
+			},
+		},
+		{
+			name: "upsert not exist usertask",
+			args: args{
+				ctx:     context.TODO(),
+				address: "0x0000000000000000000000000000000000000000",
+				taskId:  "task1",
+				state:   "completed",
+				amount:  decimal.NewFromInt(20),
+			},
+			want: model.UserTask{
+				UserAddress: "0x0000000000000000000000000000000000000000",
+				TaskID:      "task1",
+				State:       "completed",
+				Amount:      decimal.NewFromInt(20),
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mgr := Manager{db: d}
+			if err := mgr.upsert(context.TODO(), tt.args.address, tt.args.taskId, tt.args.state, tt.args.amount); err != nil {
+				t.Errorf("upsert err: %v", err)
+				return
+			}
+
+			var result model.UserTask
+			if err := d.QueryRow(
+				`SELECT "userAddress", "taskId", "state", "amount" FROM "userTask" WHERE "userAddress" = $1 AND "taskId" = $2;`,
+				tt.args.address, tt.args.taskId,
+			).Scan(
+				&result.UserAddress,
+				&result.TaskID,
+				&result.State,
+				&result.Amount,
+			); err != nil {
+				t.Errorf("scan err: %v", err)
+				return
+			}
+			assert.True(
+				t, tt.want.Amount.Equal(result.Amount), "amount not equal want: %v, got: %v", tt.want.Amount, result.Amount,
+			)
+			assert.Equal(t, tt.want.UserAddress, result.UserAddress)
+			assert.Equal(t, tt.want.TaskID, result.TaskID)
+			assert.Equal(t, tt.want.State, result.State)
+		})
+	}
+}
+
+// func TestManager_getUserTask(t *testing.T) {
+// 	godotenv.Load("../../../.env/.env")
+
+// 	d, err := testutils.GetTestDb(t, "../../../migrations")
+// 	if err != nil {
+// 		t.Errorf("setup db err: %v", err)
+// 		return
+// 	}
+// 	defer d.Close()
+
+// 	mgr := Manager{db: d}
+// }
