a7dabd3f113b65fa866c865c015e97a3c14a59f2
qwp8510 <a4793706@gmail.com>
2024-08-25 23:14:02 +0800

    test: add test GetOnboardingTask

--- pkg/service/task/task_test.go
+++ pkg/service/task/task_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 	"tradingAce/internal/testutils"
+	"tradingAce/pkg/model"
 
 	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
@@ -85,4 +86,78 @@ func TestManager_GetSharePoolTask(t *testing.T) {
 	for _, task := range models {
 		assert.True(t, slices.Contains(ids, task.ID))
 	}
+}
+
+func TestManager_CreateSharePoolTask(t *testing.T) {
+	godotenv.Load("../../../.env/.env")
+
+	d, err := testutils.GetTestDb(t, "../../../migrations")
+	if err != nil {
+		t.Errorf("setup db err: %v", err)
+		return
+	}
+	defer d.Close()
+
+	startAt, parseErr := time.Parse("2006-01-02", "2024-07-02")
+	if parseErr != nil {
+		t.Errorf("parse time err: %v", parseErr)
+		return
+	}
+
+	mgr := Manager{db: d}
+	err = mgr.CreateSharePoolTask(context.Background(), "0xabc", startAt)
+	if err != nil {
+		t.Errorf("CreateSharePoolTask fail: %s", err)
+		return
+	}
+
+	query := `
+		SELECT "id", "createdAt", "name", "pairAddress", "startAt"
+		FROM "task"
+		WHERE "name" = $1 AND "pairAddress" = $2;
+	`
+
+	var task model.Task
+	qErr := mgr.db.QueryRowContext(context.Background(), query, "share_pool", "0xabc").Scan(
+		&task.ID,
+		&task.CreatedAt,
+		&task.Name,
+		&task.PairAddress,
+		&task.StartAt,
+	)
+	if qErr != nil {
+		t.Error(qErr)
+		return
+	}
+	assert.Equal(t, "share_pool", task.Name.String)
+	assert.Equal(t, "0xabc", task.PairAddress.String)
+	assert.True(t, startAt.Equal(task.StartAt))
+}
+
+func TestManager_CreateSharePoolTaskIfExist(t *testing.T) {
+	godotenv.Load("../../../.env/.env")
+
+	d, err := testutils.GetTestDb(t, "../../../migrations")
+	if err != nil {
+		t.Errorf("setup db err: %v", err)
+		return
+	}
+	defer d.Close()
+
+	startAt, parseErr := time.Parse("2006-01-02", "2024-07-02")
+	if parseErr != nil {
+		t.Errorf("parse time err: %v", parseErr)
+		return
+	}
+
+	mgr := Manager{db: d}
+	err = mgr.CreateSharePoolTask(context.Background(), "0xabc", startAt)
+	if err != nil {
+		t.Errorf("CreateSharePoolTask fail: %s", err)
+		return
+	}
+
+	resultErr := mgr.CreateSharePoolTask(context.Background(), "0xabc", startAt)
+
+	assert.True(t, resultErr != nil)
 }
